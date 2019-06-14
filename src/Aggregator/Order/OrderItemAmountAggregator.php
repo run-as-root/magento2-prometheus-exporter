e4b7978bcb5317c9a7ba445a22926a49fffabcf1
Matthias Walter <matthias.walter@mwltr.de>
2019-06-14 15:17:49 +0200

    add OrderItem Amount and Count Aggregator

---
+++ src/Aggregator/Order/OrderItemAmountAggregator.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright see PROJECT_LICENSE.txt
+ *
+ * @see PROJECT_LICENSE.txt
+ */
+
+namespace RunAsRoot\PrometheusExporter\Aggregator\Order;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order\Item as OrderItem;
+use RunAsRoot\PrometheusExporter\Api\MetricAggregatorInterface;
+use RunAsRoot\PrometheusExporter\Service\UpdateMetricService;
+
+class OrderItemAmountAggregator implements MetricAggregatorInterface
+{
+    private const METRIC_CODE = 'magento2_orders_items_amount_total';
+
+    /**
+     * @var UpdateMetricService
+     */
+    private $updateMetricService;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    public function __construct(
+        UpdateMetricService $updateMetricService,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+        $this->updateMetricService = $updateMetricService;
+        $this->orderRepository = $orderRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
+    public function getHelp(): string
+    {
+        return 'Magento2 Order Items Amount by state';
+    }
+
+    public function getType(): string
+    {
+        return 'gauge';
+    }
+
+    /**
+     * @return bool
+     * @throws CouldNotSaveException
+     */
+    public function aggregate(): bool
+    {
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        $orderSearchResult = $this->orderRepository->getList($searchCriteria);
+
+        if ($orderSearchResult->getTotalCount() === 0) {
+            return true;
+        }
+
+        $orders = $orderSearchResult->getItems();
+
+        $grandTotals = [];
+        foreach ($orders as $order) {
+            foreach ($order->getItems() as $orderItem) {
+                /** @var $orderItem OrderItem */
+                $status = (string)$orderItem->getStatus();
+
+                if (!array_key_exists($status, $grandTotals)) {
+                    $grandTotals[$status] = 0.0;
+                }
+                $grandTotals[$status] += $orderItem->getRowTotalInclTax();
+            }
+        }
+
+        foreach ($grandTotals as $status => $grandTotal) {
+            $labels = ['status' => $status,];
+
+            $this->updateMetricService->update(self::METRIC_CODE, (string)$grandTotal, $labels);
+        }
+
+        return true;
+    }
+
+}
---
+++ src/Aggregator/Order/OrderItemCountAggregator.php
@@ -0,0 +1,95 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright see PROJECT_LICENSE.txt
+ *
+ * @see PROJECT_LICENSE.txt
+ */
+
+namespace RunAsRoot\PrometheusExporter\Aggregator\Order;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Model\Order\Item as OrderItem;
+use RunAsRoot\PrometheusExporter\Api\MetricAggregatorInterface;
+use RunAsRoot\PrometheusExporter\Service\UpdateMetricService;
+
+class OrderItemCountAggregator implements MetricAggregatorInterface
+{
+    private const METRIC_CODE = 'magento2_orders_items_count_total';
+
+    /**
+     * @var UpdateMetricService
+     */
+    private $updateMetricService;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    public function __construct(
+        UpdateMetricService $updateMetricService,
+        OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+        $this->updateMetricService = $updateMetricService;
+        $this->orderRepository = $orderRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
+    public function getHelp(): string
+    {
+        return 'Magento2 Order Items Count by state';
+    }
+
+    public function getType(): string
+    {
+        return 'gauge';
+    }
+
+    /**
+     * @return bool
+     * @throws CouldNotSaveException
+     */
+    public function aggregate(): bool
+    {
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        $orderSearchResult = $this->orderRepository->getList($searchCriteria);
+
+        if ($orderSearchResult->getTotalCount() === 0) {
+            return true;
+        }
+
+        $orders = $orderSearchResult->getItems();
+
+        $countByState = [];
+        foreach ($orders as $order) {
+            foreach ($order->getItems() as $orderItem) {
+                /** @var $orderItem OrderItem */
+                $status = (string)$orderItem->getStatus();
+
+                if (!array_key_exists($status, $countByState)) {
+                    $countByState[$status] = 0;
+                }
+                ++$countByState[$status];
+            }
+        }
+
+        foreach ($countByState as $status => $count) {
+            $labels = ['status' => $status,];
+
+            $this->updateMetricService->update(self::METRIC_CODE, (string)$count, $labels);
+        }
+
+        return true;
+    }
+
+}
