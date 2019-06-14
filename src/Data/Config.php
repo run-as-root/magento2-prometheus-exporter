7fe90eb40f65f6e8b70ad66b397a695886482ae6
David Lambauer <mail@davidlambauer.de>
2019-06-14 14:22:24 +0200

    Added Config Helper

---
+++ src/Data/Config.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * @copyright see PROJECT_LICENSE.txt
+ * @see PROJECT_LICENSE.txt
+ */
+
+namespace RunAsRoot\PrometheusExporter\Data;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Config
+{
+    /**
+     * @var StoreConfigInterface
+     */
+    private $config;
+
+    public function __construct(ScopeConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    public function isCustomerMetricsEnabled() : bool
+    {
+        return (bool)$this->config->getValue('metric_configuration/metric/customer');
+    }
+
+    public function isOrdersMetricsEnabled() : bool
+    {
+        return (bool)$this->config->getValue('metric_configuration/metric/order');
+    }
+}
