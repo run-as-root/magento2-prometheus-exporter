a20b7891f8e8652d4b0e4af0fad2a5c186d6a243
Julian van den Berkmortel <julianvdberkmortel@outlook.com>
2019-07-08 18:34:51 +0200

    Refactor the creation of scope config mock into its own method

--- src/Test/Unit/Data/ConfigUnitTest.php
+++ src/Test/Unit/Data/ConfigUnitTest.php
@@ -6,9 +6,9 @@
 
 namespace RunAsRoot\PrometheusExporter\Test\Unit\Data;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use PHPUnit\Framework\TestCase;
 use RunAsRoot\PrometheusExporter\Data\Config;
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use RunAsRoot\PrometheusExporter\Model\SourceModel\Metrics;
 
 class ConfigUnitTest extends TestCase
@@ -23,13 +23,25 @@ class ConfigUnitTest extends TestCase
         parent::setUp();
 
         /** @var ScopeConfigInterface|\PHPUnit_Framework_MockObject_MockObject $scopeConfigMock */
-        $scopeConfigMock = $this->createMock(ScopeConfigInterface::class);
-        $scopeConfigMock->method('getValue')
-            ->willReturn('magento2_orders_count_total,magento2_orders_items_amount_total,magento2_orders_items_count_total,magento_cms_page_count_total');
-
+        $scopeConfigMock = $this->createScopeConfigMock();
+        /** @var Metrics|\PHPUnit_Framework_MockObject_MockObject $metricsSource */
         $metricsSource = $this->createMetricsSourceMock();
 
         $this->sut = new Config($scopeConfigMock, $metricsSource);
+    }
+
+    private function createScopeConfigMock()
+    {
+        $mock = $this->createMock(ScopeConfigInterface::class);
+
+        $mock->method('getValue')->willReturn(implode(',', [
+            'magento2_orders_count_total',
+            'magento2_orders_items_amount_total',
+            'magento2_orders_items_count_total',
+            'magento_cms_page_count_total'
+        ]));
+
+        return $mock;
     }
 
     private function createMetricsSourceMock()
