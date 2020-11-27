7f27ae6c8c24a7bbcbe139e674abb878a120afa6
Fabian Schmengler <fs@integer-net.de>
2020-11-27 18:39:01 +0100

    Make test case signatures compatible with PHPUnit 9

--- Test/Integration/IntegrationTestAbstract.php
+++ Test/Integration/IntegrationTestAbstract.php
@@ -13,7 +13,7 @@ abstract class IntegrationTestAbstract extends TestCase
     /** @var ObjectManagerInterface */
     protected $objectManager;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
