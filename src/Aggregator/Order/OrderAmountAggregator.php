f80c3152b0e43d141a3a33543cc11ad6a1b827c4
Marvin Caspar <marvin.caspar@gmail.com>
2021-06-11 07:52:12 +0200

    Reset order count and order amount metrics
    If you have one order which is in state processing and the state changes
    after the metric aggregation to complete, than the metrics still count
    one order as processing and +1 for complete. You have to reset the
    metric values before the aggreation, otherwise you get incorrect data.

--- src/Aggregator/Order/OrderAmountAggregator.php
+++ src/Aggregator/Order/OrderAmountAggregator.php
@@ -8,8 +8,9 @@ use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Store\Api\StoreRepositoryInterface;
+use RunAsRoot\PrometheusExporter\Api\Data\MetricInterface;
 use RunAsRoot\PrometheusExporter\Api\MetricAggregatorInterface;
-use RunAsRoot\PrometheusExporter\Service\UpdateMetricService;
+use RunAsRoot\PrometheusExporter\Repository\MetricRepository;
 use RunAsRoot\PrometheusExporter\Service\UpdateMetricServiceInterface;
 use function array_key_exists;
 
@@ -17,21 +18,25 @@ class OrderAmountAggregator implements MetricAggregatorInterface
 {
     private const METRIC_CODE = 'magento_orders_amount_total';
 
-    private $updateMetricService;
-    private $orderRepository;
-    private $searchCriteriaBuilder;
-    private $storeRepository;
+    private MetricRepository $metricRepository;
+    private OrderRepositoryInterface $orderRepository;
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+    private StoreRepositoryInterface $storeRepository;
+    private UpdateMetricServiceInterface $updateMetricService;
 
     public function __construct(
-        UpdateMetricServiceInterface $updateMetricService,
+        MetricRepository $metricRepository,
         OrderRepositoryInterface $orderRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
         StoreRepositoryInterface $storeRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder
-    ) {
-        $this->updateMetricService = $updateMetricService;
+        UpdateMetricServiceInterface $updateMetricService
+    )
+    {
+        $this->metricRepository = $metricRepository;
         $this->orderRepository = $orderRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->storeRepository = $storeRepository;
+        $this->updateMetricService = $updateMetricService;
     }
 
     public function getCode(): string
@@ -51,8 +56,9 @@ class OrderAmountAggregator implements MetricAggregatorInterface
 
     public function aggregate(): bool
     {
-        $searchCriteria = $this->searchCriteriaBuilder->create();
+        $this->resetMetrics();
 
+        $searchCriteria = $this->searchCriteriaBuilder->create();
         $orderSearchResult = $this->orderRepository->getList($searchCriteria);
 
         if ($orderSearchResult->getTotalCount() === 0) {
@@ -86,12 +92,24 @@ class OrderAmountAggregator implements MetricAggregatorInterface
 
         foreach ($grandTotalsByStore as $storeCode => $grandTotals) {
             foreach ($grandTotals as $state => $grandTotal) {
-                $labels = [ 'state' => $state, 'store_code' => $storeCode ];
+                $labels = ['state' => $state, 'store_code' => $storeCode];
 
                 $this->updateMetricService->update(self::METRIC_CODE, (string)$grandTotal, $labels);
             }
         }
 
         return true;
+    }
+
+    protected function resetMetrics(): void
+    {
+        $searchCriteriaMetrics = $this->searchCriteriaBuilder->addFilter('code', self::METRIC_CODE)->create();
+        $metricsSearchResult = $this->metricRepository->getList($searchCriteriaMetrics);
+        $metrics = $metricsSearchResult->getItems();
+        /** @var MetricInterface $metric */
+        foreach ($metrics as $metric) {
+            $metric->setValue("0");
+            $this->metricRepository->save($metric);
+        }
     }
 }
