881305a663b65121f4584019746cd618df6be031
SYSTEMS\owais.ibrahim <owais.ibrahim@visionetsystems.com>
2021-09-06 16:35:19 +0500

    NewRelic Config added

--- src/Data/Config.php
+++ src/Data/Config.php
@@ -13,6 +13,12 @@ class Config
     private const CONFIG_PATH_METRICS_ENABLED = 'metric_configuration/metric/metric_status';
     private const CONFIG_PATH_AUTH_TOKEN = 'metric_configuration/security/token';
 
+    private const CONFIG_PATH_NEWRELIC_METRICS_ENABLED = 'newrelic_configuration/metric/newrelic_metric_enabled';
+    private const CONFIG_PATH_NEWRELIC_API_URL = 'newrelic_configuration/metric/api_url';
+    private const CONFIG_PATH_NEWRELIC_API_KEY = 'newrelic_configuration/metric/api_key';
+    private const CONFIG_PATH_NEWRELIC_METRIC_STATUS = 'newrelic_configuration/metric/metric_status';
+    private const CONFIG_PATH_NEWRELIC_CRON_ENABLED = 'newrelic_configuration/cron/cron_enabled';
+
     private $config;
     private $metricsSource;
 
@@ -20,6 +26,37 @@ class Config
     {
         $this->config = $config;
         $this->metricsSource = $metricsSource;
+    }
+
+    public function isNewRelicEnabled(?string $scopeCode = null): bool
+    {
+        return (bool)$this->config->isSetFlag(self::CONFIG_PATH_NEWRELIC_METRICS_ENABLED, ScopeInterface::SCOPE_STORE, $scopeCode);
+    }
+
+    public function getNewRelicMetricApiUrl(?string $scopeCode = null): string
+    {
+        return $this->config->getValue(self::CONFIG_PATH_NEWRELIC_API_URL, ScopeInterface::SCOPE_STORE, $scopeCode);
+    }
+
+    public function getNewRelicMetricApiKey(?string $scopeCode = null): string
+    {
+        return $this->config->getValue(self::CONFIG_PATH_NEWRELIC_API_KEY, ScopeInterface::SCOPE_STORE, $scopeCode);
+    }
+
+    public function getNewRelicMetricsStatus(?string $scopeCode = null): array
+    {
+        $metrics = $this->config->getValue(self::CONFIG_PATH_NEWRELIC_METRIC_STATUS, ScopeInterface::SCOPE_STORE, $scopeCode);
+
+        if ($metrics === null) {
+            return $this->getDefaultMetrics();
+        }
+
+        return explode(',', $metrics);
+    }
+
+    public function isNewRelicCronEnabled(?string $scopeCode = null): bool
+    {
+        return (bool)$this->config->isSetFlag(self::CONFIG_PATH_NEWRELIC_API_KEY, ScopeInterface::SCOPE_STORE, $scopeCode);
     }
 
     public function getMetricsStatus(?string $scopeCode = null): array
