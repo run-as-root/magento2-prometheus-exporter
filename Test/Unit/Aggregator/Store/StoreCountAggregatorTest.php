44b753aeefa78fb121e49a79a859e510ac5f0141
Oleksandr Kravchuk <o.kravchuk@vconnect.dk>
2021-11-13 14:22:41 +0200

    run-as-root/magento2-prometheus-exporter#24: - adjust 'magento_store_count_total' metric; - add status label to stores count; - adjust unit tests coverage;

--- Test/Unit/Aggregator/Store/StoreCountAggregatorTest.php
+++ Test/Unit/Aggregator/Store/StoreCountAggregatorTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RunAsRoot\PrometheusExporter\Test\Unit\Aggregator\Module;
 
+use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Api\StoreRepositoryInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -12,6 +13,8 @@ use RunAsRoot\PrometheusExporter\Service\UpdateMetricService;
 
 final class StoreCountAggregatorTest extends TestCase
 {
+    private const METRIC_CODE = 'magento_store_count_total';
+
     /** @var StoreCountAggregator */
     private $sut;
 
@@ -21,7 +24,7 @@ final class StoreCountAggregatorTest extends TestCase
     /** @var MockObject|StoreRepositoryInterface */
     private $storeRepository;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->updateMetricService = $this->createMock(UpdateMetricService::class);
         $this->storeRepository     = $this->createMock(StoreRepositoryInterface::class);
@@ -34,15 +37,26 @@ final class StoreCountAggregatorTest extends TestCase
 
     public function testAggregate(): void
     {
+        $store1 = $this->createMock(StoreInterface::class);
+        $store2 = $this->createMock(StoreInterface::class);
+        $store3 = $this->createMock(StoreInterface::class);
+
+        $store1->expects($this->once())->method('getIsActive')->willReturn(1);
+        $store2->expects($this->once())->method('getIsActive')->willReturn(0);
+        $store3->expects($this->once())->method('getIsActive')->willReturn(1);
+
         $this->storeRepository
             ->expects($this->once())
             ->method('getList')
-            ->willReturn(['a', 'b', 'c']);
+            ->willReturn([$store1, $store2, $store3]);
 
         $this->updateMetricService
-            ->expects($this->once())
+            ->expects($this->exactly(2))
             ->method('update')
-            ->with(...['magento_store_count_total', '3']);
+            ->withConsecutive(
+                [self::METRIC_CODE, '2', ['status' => 'enabled']],
+                [self::METRIC_CODE, '1', ['status' => 'disabled']],
+            );
 
         $this->sut->aggregate();
     }
