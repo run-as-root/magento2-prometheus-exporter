4e0d21655ca4d6158329a1a44ebf99b578d9534c
David Lambauer <david@run-as-root.sh>
2021-11-14 22:04:45 +0100

    Added Area Code and Fixed description

--- src/Console/Command/ListMetricsCommand.php
+++ src/Console/Command/ListMetricsCommand.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace RunAsRoot\PrometheusExporter\Console\Command;
 
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\App\Area;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
 use RunAsRoot\PrometheusExporter\Api\Data\MetricInterface;
 use RunAsRoot\PrometheusExporter\Api\MetricRepositoryInterface;
 use Symfony\Component\Console\Command\Command;
@@ -15,22 +18,25 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ListMetricsCommand extends Command
 {
     private const COMMAND_NAME = 'run_as_root:metric:list';
-    private const COMMAND_DESCRIPTION = 'Check Project Status at Eurotext. Will update project status in Magento.';
+    private const COMMAND_DESCRIPTION = 'Lists all metrics currently collected in the db.';
 
-    private $metricRepository;
-    private $searchCriteriaBuilder;
+    private MetricRepositoryInterface $metricRepository;
+    private SearchCriteriaBuilder $searchCriteriaBuilder;
+    private State $state;
 
     public function __construct(
         MetricRepositoryInterface $metricRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        State $state
     ) {
         parent::__construct();
 
         $this->metricRepository = $metricRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->state = $state;
     }
 
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName(self::COMMAND_NAME);
         $this->setDescription(self::COMMAND_DESCRIPTION);
@@ -40,6 +46,11 @@ class ListMetricsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        try {
+            $this->state->setAreaCode(Area::AREA_GLOBAL);
+        } catch (LocalizedException $e) {
+            $output->writeln('<error>Cannot set area to global<error/>');
+        }
         $searchCriteria = $this->searchCriteriaBuilder->create();
 
         $searchResults = $this->metricRepository->getList($searchCriteria);
