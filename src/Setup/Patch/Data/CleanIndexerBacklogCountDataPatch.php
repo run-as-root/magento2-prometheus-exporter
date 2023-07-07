b4c6e7e1c9608b32977b4ad1ab201fb451ede8f5
Vladyslav Sikailo <vladyslav.sikailo@run-as-root.sh>
2023-07-07 11:33:13 +0300

    add cleanup data patch to delete metrics with wrong structure

--- src/Setup/Patch/Data/CleanIndexerBacklogCountDataPatch.php
+++ src/Setup/Patch/Data/CleanIndexerBacklogCountDataPatch.php
@@ -1,6 +1,46 @@
 <?php
 
-class CleanIndexerBacklogCountDataPatch
+declare(strict_types=1);
+
+namespace RunAsRoot\PrometheusExporter\Setup\Patch\Data;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use RunAsRoot\PrometheusExporter\Api\Data\MetricInterface;
+use RunAsRoot\PrometheusExporter\Repository\MetricRepository;
+
+class CleanIndexerBacklogCountDataPatch implements DataPatchInterface
 {
-    //todo:
+    private const METRIC_CODE = 'magento_indexer_backlog_count_total';
+
+    public function __construct(
+        private readonly MetricRepository $metricRepository,
+        private readonly SearchCriteriaBuilder $searchCriteriaBuilder
+    ) {
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * @throws CouldNotDeleteException
+     */
+    public function apply(): void
+    {
+        $searchCriteriaMetrics = $this->searchCriteriaBuilder->addFilter('code', self::METRIC_CODE)->create();
+        $metricsSearchResult = $this->metricRepository->getList($searchCriteriaMetrics);
+        $metrics = $metricsSearchResult->getItems();
+        /** @var MetricInterface $metric */
+        foreach ($metrics as $metric) {
+            $this->metricRepository->delete($metric);
+        }
+    }
 }
